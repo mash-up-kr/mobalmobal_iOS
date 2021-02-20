10d1902f1c789b5e6e9f8fa9e24dab72f336c4b1
zhilogs <jaeheekim970327@gmail.com>
2021-02-20 17:52:46 +0900

    🎉 begin MobalMobal project

---
+++ MobalMobal/MobalMobal/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MobalMobal
+//
+//  Created by 김재희 on 2021/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
