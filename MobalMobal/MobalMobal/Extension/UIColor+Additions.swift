477eb7bb988862b14d1c50dedfb3133dafffcc64
seoyoung <seoyoung_00@hufs.ac.kr>
2021-02-28 01:28:01 +0900

    🎨 add background UIColor Extension

---
+++ MobalMobal/MobalMobal/Extension/UIColor+Additions.swift
@@ -0,0 +1,17 @@
+//
+//  UIColor+Additions.swift
+//  Mobal iOS
+//
+//  Generated on Zeplin. (2021. 2. 28.).
+//  Copyright (c) 2021 __MyCompanyName__. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+  @nonobjc class var darkGrey: UIColor {
+    return UIColor(red: 21.0 / 255.0, green: 21.0 / 255.0, blue: 22.0 / 255.0, alpha: 1.0)
+  }
+
+}
