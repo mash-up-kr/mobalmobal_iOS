621189561fa9b2a1183bb798d1f72c43e67b4ccd
tngusmiso <k1miso012@gmail.com>
2021-03-07 16:57:08 +0900

    🚧 (#29) Apple & Firebase Auth 연동

--- MobalMobal/MobalMobal/Login/LoginViewController.swift
+++ MobalMobal/MobalMobal/Login/LoginViewController.swift
@@ -5,6 +5,7 @@
 //  Created by 임수현 on 2021/02/27.
 //
 import AuthenticationServices
+import CryptoKit
 import FBSDKLoginKit
 import Firebase
 import GoogleSignIn
@@ -32,6 +33,9 @@ class LoginViewController: UIViewController {
         let button: UIView = CustomLoginButton(title: "Apple로 로그인하기", iconName: "appleLogo")
         return button
     }()
+    
+    // MARK: - Properties
+    fileprivate var currentNonce: String?
     
     // MARK: - Initializer
     private func setSuperview() {
@@ -66,8 +70,8 @@ class LoginViewController: UIViewController {
         let facebookLoginTap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(clickFacebookLoginButton))
         facebookButton.addGestureRecognizer(facebookLoginTap)
         
-         let appleLoginTap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(clickAppleLoginButton))
-         appleButton.addGestureRecognizer(appleLoginTap)
+        let appleLoginTap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(clickAppleLoginButton))
+        appleButton.addGestureRecognizer(appleLoginTap)
     }
     
     // MARK: - Life Cycle
@@ -128,6 +132,8 @@ extension LoginViewController: GIDSignInDelegate {
         GIDSignIn.sharedInstance().signIn()
     }
     
+    // MARK: Google Guideline
+    
     // 구글 로그인 연동 시도 했을 시 호출
     func sign(_ signIn: GIDSignIn?, didSignInFor user: GIDGoogleUser?, withError error: Error?) {
         if let error: Error = error {
@@ -147,7 +153,7 @@ extension LoginViewController: GIDSignInDelegate {
         let credential: AuthCredential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
         loginWithFirebase(credential: credential)
     }
-
+    
     // 구글 로그인 연동 해제 시 호출
     func sign(_ signIn: GIDSignIn?, didDisconnectWith user: GIDGoogleUser?, withError error: Error?) {
         print("🐻 GoogleLogin :: disconnected 🐻")
@@ -186,36 +192,104 @@ extension LoginViewController {
 // MARK: - Apple
 extension LoginViewController: ASAuthorizationControllerPresentationContextProviding, ASAuthorizationControllerDelegate {
     private func loginWithApple() {
-        let appleIDProvider = ASAuthorizationAppleIDProvider()
-        let request = appleIDProvider.createRequest()
+        let nonce: String = randomNonceString()
+        currentNonce = nonce
+        
+        let appleIDProvider: ASAuthorizationAppleIDProvider = ASAuthorizationAppleIDProvider()
+        let request: ASAuthorizationAppleIDRequest = appleIDProvider.createRequest()
         request.requestedScopes = [.fullName, .email]
-            
-        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        request.nonce = sha256(nonce) // 애플 로그인 요청 시 nonce의 SHA256 해시를 전송해야 한다.
+        
+        let authorizationController: ASAuthorizationController = ASAuthorizationController(authorizationRequests: [request])
         authorizationController.delegate = self
         authorizationController.presentationContextProvider = self
         authorizationController.performRequests()
     }
     
-    // Apple login 모달 창 띄우기
-    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-        return self.view.window!
+    // MARK: Firebase Guideline
+    
+    /// 랜덤 nonce를 생성하는 함수.
+    /// 애플에 로그인 요청 시 nonce의 SHA256 해시를 전송한다.
+    /// 로그인 응답으로 들어온 nonce를 통해 ID 토큰이 명시적으로 부여되었는지 확인한다.
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: [Character] = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result: String = ""
+        var remainingLength: Int = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode: Int32 = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("🐻 Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        return result
     }
     
-    // Apple ID 연동 성공 시
+    /// 문자열의 SHA256 해시를 만드는 함수.
+    private func sha256(_ input: String) -> String {
+        let inputData: Data = Data(input.utf8)
+        let hashedData: SHA256Digest = SHA256.hash(data: inputData)
+        let hashString: String = hashedData.compactMap {
+            String(format: "%02x", $0)
+        }.joined()
+        
+        return hashString
+    }
+    
+    // MARK: Apple Guideline
+    
+    // Apple login 모달 창 띄우기
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        self.view.window!
+    }
+    
+    // Apple ID 연동 성공 시 호출
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
         switch authorization.credential {
         // Apple ID
         case let appleIDCredential as ASAuthorizationAppleIDCredential:
+            
+            guard let nonce: String = currentNonce else {
+                print("🐻 AppleLogin :: nonce 없음")
+                return
+            }
+            guard let token: Data = appleIDCredential.identityToken else {
+                print("🐻 AppleLogin :: Unable to fetch identity token")
+                return
+            }
+            guard let tokenString: String = String(data: token, encoding: .utf8) else {
+                print("🐻 AppleLogin :: Unable to serialize token string from data: \(token.debugDescription)")
+                return
+            }
+            print("🐻 AppleLogin :: Token: \(tokenString)")
             print("🐻 AppleLogin :: ID: \(appleIDCredential.user)")
-            print("🐻 AppleLogin :: Name: \(appleIDCredential.fullName?.description ?? "정보 없음")")
-            print("🐻 AppleLogin :: Email: \(appleIDCredential.email ?? "정보 없음")")
+            
+            // 토큰 받아오는 데 성공하면 파이어베이스로 인증
+            let credential: AuthCredential = OAuthProvider.credential(withProviderID: "apple.com", idToken: tokenString, rawNonce: nonce)
+            loginWithFirebase(credential: credential)
             
         default:
             break
         }
     }
-        
-    // Apple ID 연동 실패 시
+    
+    // Apple ID 연동 실패 시 호출
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
         print("🐻 AppleLogin :: 로그인 실패 \(error.localizedDescription)")
     }
