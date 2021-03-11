b058c76d90bd819aaac3e0704b1bc748709b1958
tngusmiso <k1miso012@gmail.com>
2021-03-11 23:50:19 +0900

    ✨ (#63) Set screen orientation portrait only

--- MobalMobal/MobalMobal/AppDelegate.swift
+++ MobalMobal/MobalMobal/AppDelegate.swift
@@ -11,6 +11,8 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var shoudSupportAllOrientation: Bool = false
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
@@ -30,6 +32,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         return true
     }
+    
+    // screen orientation
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        if shoudSupportAllOrientation {
+            return .all
+        }
+        return .portrait
+    }
 
     // MARK: UISceneSession Lifecycle
 
@@ -44,6 +54,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
 }
 
 // MARK: - UNUserNotificationCenterDelegate
