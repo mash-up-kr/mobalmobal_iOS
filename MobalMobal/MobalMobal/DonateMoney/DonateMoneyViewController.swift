670e274905aa973f94ee165cf953b9a687d67726
tngusmiso <k1miso012@gmail.com>
2021-03-12 17:49:13 +0900

    💄 (#31) 후원페이지 tableview 설정 - backgroundColor 설정 - delegate, datasource 설정 - headerView 설정

--- MobalMobal/MobalMobal/DonateMoney/DonateMoneyViewController.swift
+++ MobalMobal/MobalMobal/DonateMoney/DonateMoneyViewController.swift
@@ -9,21 +9,71 @@ import UIKit
 
 class DonateMoneyViewController: UIViewController {
     // MARK: - UIComponents
-    private let label: UILabel = UILabel()
+    private lazy var tableView: UITableView = {
+        let tableView: UITableView = UITableView()
+        tableView.backgroundColor = .darkGreyTwo
+        tableView.separatorStyle = .none
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
     
     // MARK: - Properties
-        
+    private let headerString: String = "후원"
+    private let moneyStrings: [String] = ["1,000원", "2,000원", "5,000원", "10,000원", "50,000원", "100,000원", "직접입력"]
+    
     // MARK: - Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .backgroundColor
-        view.addSubview(label)
-        label.text = "후원하기"
-        label.textColor = .white
-        label.snp.makeConstraints { make in make.center.equalToSuperview() }
+        view.backgroundColor = .clear
+    }
+    
+    override func updateViewConstraints() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.height.equalTo(458)
+        }
+        super.updateViewConstraints()
     }
     
     // MARK: - Actions
     
     // MARK: - Methods
+}
+
+// MARK: - Protocols
+extension DonateMoneyViewController: UITableViewDelegate { }
+
+extension DonateMoneyViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView: UIView = UIView()
+        let headerLabel: UILabel = {
+            let label: UILabel = UILabel()
+            label.text = headerString
+            label.font = .spoqaHanSansNeo(ofSize: 18, weight: .medium)
+            label.textColor = .white
+            return label
+        }()
+        
+        headerView.addSubview(headerLabel)
+        headerLabel.snp.makeConstraints { make in make.center.equalToSuperview() }
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        62
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        moneyStrings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell()
+        cell.backgroundColor = .darkGreyTwo
+        cell.largeContentTitle = moneyStrings[indexPath.row]
+        return cell
+    }
 }
