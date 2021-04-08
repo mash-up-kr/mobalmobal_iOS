70ed8b2bca4eec641d20e7d95e69bd4ee8465675
tngusmiso <k1miso012@gmail.com>
2021-04-08 23:43:36 +0900

    🐛 [ViewController] 수현버튼 -  Navigation Controller push 방식 수정

--- MobalMobal/MobalMobal/Login/LoginViewController.swift
+++ MobalMobal/MobalMobal/Login/LoginViewController.swift
@@ -46,10 +46,13 @@ class LoginViewController: UIViewController {
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .backgroundColor
-
         setActions()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
     }
     
     override func updateViewConstraints() {
